e53e59b545b25dc8e4c2b4b3062ca8dab10ed252
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2022-05-30 14:30:03 +0200

    Update code.py

--- tone_circuitpython/code.py
+++ tone_circuitpython/code.py
@@ -21,17 +21,19 @@ except ImportError:
     except ImportError:
         pass  # not always supported by every board!
 tone_volume = 0.2  # Increase this to increase the volume of the tone.
-FREQUENCY = 1000  # Hz
+FREQUENCY = 1720  # Hz
 SAMPLERATE = 48000  # 8000 samples/second, recommended!
 
 button = digitalio.DigitalInOut(board.BUTTON_A)
 button.switch_to_input(pull=digitalio.Pull.DOWN)
 
-# Generate one period of sine wav.
-length = SAMPLERATE // FREQUENCY
-sine_wave = array.array("H", [0] * length)
-for i in range(length):
-    sine_wave[i] = int(math.sin(math.pi * 2 * i / length) * tone_volume * (2 ** 15) + 2 ** 15)
+def generate_sin(sample_rate, frequency, volume):
+    # Generate one period of sine wav.
+    length = sample_rate // frequency
+    sine_wave = array.array("H", [0] * length)
+    for i in range(length):
+        sine_wave[i] = int(math.sin(math.pi * 2 * i / length) * volume * (2 ** 15) + 2 ** 15)
+    return RawSample(sine_wave, sample_rate=sample_rate)
 
 # Enable the speaker
 speaker_enable = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
@@ -39,7 +41,7 @@ speaker_enable.direction = digitalio.Direction.OUTPUT
 speaker_enable.value = True
 
 audio = AudioOut(board.SPEAKER)
-sine_wave_sample = RawSample(sine_wave, sample_rate=SAMPLERATE)
+sine_wave_sample = generate_sin(SAMPLERATE, FREQUENCY, tone_volume)
 
 # A single sine wave sample is hundredths of a second long. If you set loop=False, it will play
 # a single instance of the sample (a quick burst of sound) and then silence for the rest of the
@@ -47,7 +49,4 @@ sine_wave_sample = RawSample(sine_wave, sample_rate=SAMPLERATE)
 # continuously for the duration of the time.sleep().
 
 while True:
-    if button.value:  # button is pushed
-        audio.play(sine_wave_sample, loop=True)  # Play the single sine_wave sample continuously...
-        time.sleep(2)  # for the duration of the sleep (in seconds)
-        audio.stop()  # and then stop.
+    if button.value:  #and then stop.
