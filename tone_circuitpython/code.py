c5ad45d5368cbf9fefc3c62cff39be14ab5d5423
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2022-05-30 17:26:31 +0200

    Update code.py

--- tone_circuitpython/code.py
+++ tone_circuitpython/code.py
@@ -32,7 +32,7 @@ def generate_sin(sample_rate, frequency, volume):
     length = sample_rate // frequency
     sine_wave = array.array("H", [0] * length)
     for i in range(length):
-        sine_wave[i] = int(math.sin(math.pi * 2 * i / length) * volume * (2 ** 15) + 2 ** 15)
+        sine_wave[i] = int(math.sin(math.pi * 2 * i * (frequency / sample_rate)) * volume * (2 ** 15) + 2 ** 15)
     return RawSample(sine_wave, sample_rate=sample_rate)
 
 # Enable the speaker
@@ -41,12 +41,26 @@ speaker_enable.direction = digitalio.Direction.OUTPUT
 speaker_enable.value = True
 
 audio = AudioOut(board.SPEAKER)
-sine_wave_sample = generate_sin(SAMPLERATE, FREQUENCY, tone_volume)
 
 # A single sine wave sample is hundredths of a second long. If you set loop=False, it will play
 # a single instance of the sample (a quick burst of sound) and then silence for the rest of the
 # duration of the time.sleep(). If loop=True, it will play the single instance of the sample
 # continuously for the duration of the time.sleep().
 
+red = [255, 255, 204, 51, 0, 0, 0, 51, 204, 255]
+green = [0, 153, 255, 255, 255, 255, 102, 0, 0, 0]
+blue = [0, 0, 0, 0, 102, 255, 255, 255, 255, 153]
+tone_time = 2
 while True:
-    if button.value:  #and then stop.
+    if button.value:  # button is pushed
+        time.sleep(2)
+        for i in range(90, 90+16):            
+            start = time.monotonic()
+            freq = round(440 * 2**((i-69) / 12.0))
+            print("%d Hz" % freq)
+            sine_wave_sample = generate_sin(SAMPLERATE, freq, tone_volume)
+            audio.play(sine_wave_sample, loop=True)  # Play the single sine_wave sample continuously...
+            time.sleep(max(0, tone_time - (time.monotonic()-start)))  # for the duration of the sleep (in seconds)
+            audio.stop()  # and then stop.
+            time.sleep(0.1)
+
